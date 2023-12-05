5d7187555a5a8072cec406657c355a3a1b224e0b
akarabek2 <143294407+akarabek2@users.noreply.github.com>
2023-12-05 16:40:21 -0600

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,14 @@
+function init(){
+	let canvas = document.getElementById("canvas");
+  let location = new google.maps.LatLng(19.56452649083203, -99.34831743042731);
+  let mapOptions = {
+    center: location,
+    zoom: 8,
+  };
+
+  let map = new google.maps.Map(canvas, mapOptions);
+	
+
+}
+
+google.maps.event.addDomListener(window, 'load', init);
